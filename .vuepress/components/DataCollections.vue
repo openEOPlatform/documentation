e820f820a4157348e96aa5719c38a6d131639d53
Matthias Mohr <m.mohr@uni-muenster.de>
2021-08-31 13:14:52 +0200

    Preview in openEO Platform Editor

--- .vuepress/components/DataCollections.vue
+++ .vuepress/components/DataCollections.vue
@@ -2,7 +2,12 @@
 	<section>
 		<p v-if="!collections">Loading data...</p>
 		<Collections v-else :collections="collections" :showKeywords="true" :searchTerm="searchTerm" :loadAdditionalData="loadCollection">
-
+			<template #collection-before-description="slot">
+				<form class="editor-preview" v-if="hasPreview(slot.data)" target="_blank" action="https://editor.openeo.cloud">
+					<input type="hidden" name="preview-collection" :value="slot.data.id" />
+					<button type="submit">Show Preview in openEO Platform Editor</button>
+				</form>
+			</template>
 		</Collections>
 	</section>
 </template>
@@ -10,6 +15,7 @@
 <script>
 const axios = require('axios');
 import Collections from '@openeo/vue-components/components/Collections.vue';
+import Utils from '@openeo/vue-components/utils';
 
 export default {
 	name: 'DataCollections',
@@ -36,7 +42,20 @@ export default {
 		async loadCollection(_, id) {
 			let res = await axios('https://openeocloud.vito.be/openeo/1.0.0/collections/' + id);
 			return res.data;
+		},
+		hasPreview(stac) {
+			if (Utils.isObject(stac) && Array.isArray(stac.links)) {
+				let link = stac.links.find(link => Utils.isObject(link) && typeof link.rel === 'string' && ['xyz','wmts'].includes(link.rel.toLowerCase()));
+				return Boolean(link);
+			}
+			return false;
 		}
 	}
 };
 </script>
+
+<style scoped>
+.editor-preview {
+	margin: 1em 0;
+}
+</style>
